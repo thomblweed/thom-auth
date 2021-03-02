0a1f1fac835514580753126f0088b62d7543e5c2
Thomas Newman <thomblweed@gmail.com>
2021-03-02 22:34:16 +0000

    oak cors added with temp config

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import { Application } from "https://deno.land/x/oak@v6.5.0/mod.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.1/mod.ts";
 import { yellow } from "https://deno.land/std@0.88.0/fmt/colors.ts";
 
 import { errorHandler } from "./src/handlers/errors/error-handler.ts";
@@ -7,6 +8,12 @@ import router from "./src/router/router.ts";
 const app: Application = new Application();
 const PORT: number = 3001;
 
+app.use(
+  oakCors({
+    origin: "http://localhost:1234",
+    credentials: true,
+  }),
+);
 app.use(errorHandler);
 app.use(router.routes());
 app.use(router.allowedMethods());
