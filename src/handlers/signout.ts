d36114f784019b0cbf0d0c8b11a61c70fef965a3
Thomas <thomblweed@gmail.com>
2021-06-03 11:20:41 +0100

    oak version specific update

--- src/handlers/signout.ts
+++ src/handlers/signout.ts
@@ -1,4 +1,4 @@
-import { Context } from "https://deno.land/x/oak/mod.ts";
+import { Context } from "https://deno.land/x/oak@v6.5.0/mod.ts";
 
 const signout = async (ctx: Context) => {
   ctx.cookies.delete("thom-jwt");
